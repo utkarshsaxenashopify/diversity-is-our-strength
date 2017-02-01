d04fe5ca3d75866ac3c3429942677052d4fb3627
Ismail Syed <ismail.syed@shopify.com>
2017-02-01 08:10:51 -0500

    Update search results button click

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,7 @@ SimpleJekyllSearch({
   searchInput: document.getElementById('search-input'),
   resultsContainer: document.getElementById('results-container'),
   json: base_url + '/search.json',
-  searchResultTemplate: '<li><a href="{social}" title="{title}">{title}</a></li>',
+  searchResultTemplate: '<li><a href="{url}" title="{title}">{title}</a></li>',
   noResultsText: '<li class="no-results">No results found :(</li>',
   limit: 10,
   fuzzy: false
